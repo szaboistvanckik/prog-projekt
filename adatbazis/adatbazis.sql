8896383320ab03eba364913e0cdf038fb2b6add3
szabo.istvan <szabo.istvan@ckik.hu>
2025-06-01 15:04:20 +0200

    Projekt-

--- adatbazis/adatbazis.sql
+++ adatbazis/adatbazis.sql
@@ -9,8 +9,3 @@ CREATE TABLE IF NOT EXISTS Players (
     rank VARCHAR(255),
     has_mic BOOLEAN
 );
-
-INSERT INTO Players(name, level, hs_percent, rank, has_mic) VALUES
-('ShadowReign', 46, 52.22, 'Bronze', false),
-('BlitzFury', 113, 24.08, 'Ascendant', false),
-('AceHunter', 147, 24.2, 'Bronze', true);
